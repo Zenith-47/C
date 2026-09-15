89ae1bf0b965ed5bd8b58c835084fa88cb9e695d
Himanshu Awasthi <awasthih1509@gmail.com>
2026-09-15 13:37:30 +0530

    Add files via upload

---
+++ descend.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int count = 10;
+
+    while (count >= 1) {
+        printf("%d\n", count);
+        count--; // Decrement the counter
+    }
+
+    return 0;
+}
